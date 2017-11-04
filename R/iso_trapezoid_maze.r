ed715870cad56d772a9aa6a09bfc764fb1f30622
Steven Pav <shabbychef@gmail.com>
2017-11-03 20:55:52 -0700

    iso_trapezoid has methods now.
    can be four_trapezoids or one_ear.

--- R/iso_trapezoid_maze.r
+++ R/iso_trapezoid_maze.r
@@ -42,9 +42,16 @@
 #' @template return-none
 #' @param depth the depth of recursion. Should be integer. This controls the
 #' side length.
-#'
-#' @note currently there is only one method for drawing an equilateral
-#' trapezoid, though there may be more than one method in the future.
+#' @param method there are many ways to recursive draw an isosceles
+#' trapezoid.  The following values are acceptable:
+#' \describe{
+#' \item{four_trapezoids}{Four isosceles trapezoids are packed around each
+#' other with a \sQuote{bone} between them.}
+#' \item{one_ear}{A parallelogram is placed next to an equilateral triangle
+#' (an \sQuote{ear}). Note this method is acceptable when depth is
+#' not an integer.}
+#' \item{random}{A method is chosen uniformly at random.}
+#' }
 #'
 #' @examples 
 #' \dontrun{
@@ -55,46 +62,93 @@
 #' turtle_init(1000,1000)
 #' turtle_hide() 
 #' turtle_do({
-#' iso_trapezoid_maze(depth=3,20,clockwise=TRUE,draw_boundary=TRUE,boundar_holes=3,boundary_hole_color=c('red','clear','green')
+#' iso_trapezoid_maze(depth=3,20,clockwise=TRUE,draw_boundary=TRUE,boundary_holes=3)
+#' })
+#'
+#' turtle_init(2000,2000)
+#' turtle_hide() 
+#' turtle_up()
+#' turtle_do({
+#' 	iso_trapezoid_maze(depth=log2(15),20,clockwise=!TRUE,draw_boundary=TRUE,boundary_holes=c(1,2,3),method='one_ear',boundary_hole_color=c('red','clear','green'))
 #' })
 #' }
 #' @export
 iso_trapezoid_maze <- function(depth,unit_len=4L,clockwise=TRUE,start_from=c('midpoint','corner'),
-															draw_boundary=FALSE,num_boundary_holes=2,boundary_lines=TRUE,boundary_holes=NULL,boundary_hole_color=NULL,
-															end_side=1) {
+															 method=c('four_trapezoids','one_ear','random'),
+															 draw_boundary=FALSE,num_boundary_holes=2,boundary_lines=TRUE,boundary_holes=NULL,boundary_hole_color=NULL,
+															 end_side=1) {
 	start_from <- match.arg(start_from)
 
-	num_segs <- 2^depth
+	# check for off powers of two
+	non_two <- ! .near_integer(depth)
+	num_segs <- round(2^depth)
+
+	if (non_two) { 
+		method <- 'one_ear' 
+	} else if (method=='random') {
+		method <- sample(c('four_trapezoids','one_ear'),1)
+	}
+
 	multiplier <- ifelse(clockwise,1,-1)
 
 	if (start_from=='corner') { turtle_backward(dist=unit_len * num_segs) }
 
 	if (depth > 0) {
-		# recurse.
-		magic_ratio <- sqrt(3) / 4
+		my_method <- method
+		switch(my_method,
+			four_trapezoids={
+				# recurse.
+				magic_ratio <- sqrt(3) / 4
 
-		turtle_up()
-		.turn_right(multiplier * 90)
-		turtle_forward(num_segs * unit_len * magic_ratio)
-		.turn_left(multiplier * 90)
-		holey_bone(unit_len,num_segs/2)
-		.turn_left(multiplier * 90)
-		turtle_forward(num_segs * unit_len * magic_ratio)
-		.turn_right(multiplier * 90)
+				turtle_up()
+				.turn_right(multiplier * 90)
+				turtle_forward(num_segs * unit_len * magic_ratio)
+				.turn_left(multiplier * 90)
+				holey_bone(unit_len,num_segs/2)
+				.turn_left(multiplier * 90)
+				turtle_forward(num_segs * unit_len * magic_ratio)
+				.turn_right(multiplier * 90)
 
-		if (depth > 1) {
-			iso_trapezoid_maze(depth-1,unit_len,clockwise=clockwise,draw_boundary=FALSE)
-			turtle_forward(num_segs * unit_len)
-			.turn_right(multiplier * 120)
-			for (iii in c(1:3)) {
-				turtle_forward(num_segs * unit_len/2)
-				iso_trapezoid_maze(depth-1,unit_len,clockwise=clockwise,draw_boundary=FALSE)
-				turtle_forward(num_segs * unit_len/2)
-				.turn_right(multiplier * 60)
-			}
-			.turn_right(multiplier * 60)
-			turtle_forward(num_segs * unit_len)
-		}
+				if (depth > 1) {
+					iso_trapezoid_maze(depth-1,unit_len,clockwise=clockwise,draw_boundary=FALSE)
+					turtle_forward(num_segs * unit_len)
+					.turn_right(multiplier * 120)
+					for (iii in c(1:3)) {
+						turtle_forward(num_segs * unit_len/2)
+						iso_trapezoid_maze(depth-1,unit_len,clockwise=clockwise,draw_boundary=FALSE)
+						turtle_forward(num_segs * unit_len/2)
+						.turn_right(multiplier * 60)
+					}
+					.turn_right(multiplier * 60)
+					turtle_forward(num_segs * unit_len)
+				}
+			},
+			one_ear={
+				coinflip <- sample.int(n=2,size=1)
+				if (coinflip==1) {
+					parallelogram_maze(unit_len,height=num_segs,width=num_segs,angle=120,clockwise=clockwise,
+														 method='random',start_from='midpoint',
+														 draw_boundary=TRUE,num_boundary_holes=NULL,boundary_lines=c(4),boundary_holes=4,
+														 end_side=4)
+					eq_triangle_maze(unit_len,depth=log2(num_segs),clockwise=!clockwise,
+														 method='random',start_from='midpoint',
+														 draw_boundary=FALSE,end_side=2)
+					turtle_right(180)
+					turtle_forward(num_segs * unit_len/2)
+				} else {
+					turtle_forward(num_segs * unit_len/2)
+					eq_triangle_maze(unit_len,depth=log2(num_segs),clockwise=clockwise,
+														 method='random',start_from='midpoint',
+														 draw_boundary=FALSE,end_side=3)
+					parallelogram_maze(unit_len,height=num_segs,width=num_segs,angle=60,clockwise=!clockwise,
+														 method='random',start_from='midpoint',
+														 draw_boundary=TRUE,num_boundary_holes=NULL,boundary_lines=c(1),boundary_holes=1,
+														 end_side=2)
+					turtle_right(180)
+					turtle_forward(num_segs * unit_len/2)
+				}
+			})
+
 	}
 	if (draw_boundary) {
 		holes <- .interpret_boundary_holes(boundary_holes,num_boundary_holes,nsides=4)
