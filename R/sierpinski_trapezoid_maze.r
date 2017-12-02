ae304bc81b12494e334b3be691c0573016e55d3e
Steven Pav <shabbychef@gmail.com>
2017-12-01 22:01:02 -0800

    sierpinski trapezoid
    now with control over which way to flip colors, can result in
    traditional Sierpinski or a Hexaflake.

--- R/sierpinski_trapezoid_maze.r
+++ R/sierpinski_trapezoid_maze.r
@@ -57,6 +57,11 @@
 #' side length: three sides have \code{round(2^depth)} segments
 #' of length \code{unit_len}, while the long side is twice as long.
 #' \code{depth} need not be integral.
+#' @param flip_color_parts a numerical array which can contain values 1
+#' through 4. Those parts of the maze, when drawn recursively, have 
+#' their colors flipped. A value of \code{3} corresponds to a traditional
+#' Sierpinski triangle, while \code{1} corresponds to a Hexaflake.
+#' Values of \code{2} or \code{4} look more like dragon mazes.
 #'
 #' @examples 
 #'
@@ -74,10 +79,46 @@
 #' 														boundary_lines=c(2,3,4),boundary_holes=3)
 #' })
 #' 
+#' # stack some trapezoids!
+#' require(TurtleGraphics)
+#' turtle_init(750,900,mode='clip')
+#' turtle_hide()
+#' turtle_up()
+#' turtle_do({
+#'   turtle_setpos(25,450)
+#'   turtle_setangle(0)
+#' 	blines <- c(1,2,4)
+#' 	for (dep in seq(from=5,to=0)) {
+#' 		sierpinski_trapezoid_maze(unit_len=13,depth=dep,color1='black',color2='green',
+#' 															flip_color_parts=2,
+#' 															clockwise=TRUE,boundary_lines=blines,draw_boundary=TRUE,boundary_holes=c(1,3),
+#' 															end_side=3)
+#' 		turtle_right(180)
+#' 		blines <- c(1,2,4)
+#' 	}
+#' })
+#'
+#' require(TurtleGraphics)
+#' turtle_init(750,900,mode='clip')
+#' turtle_hide()
+#' turtle_up()
+#' turtle_do({
+#'   turtle_setpos(25,450)
+#'   turtle_setangle(0)
+#' 	blines <- c(1,2,4)
+#' 	for (dep in seq(from=5,to=0)) {
+#' 		sierpinski_trapezoid_maze(unit_len=13,depth=dep,color1='black',color2='green',
+#' 															flip_color_parts=3,
+#' 															clockwise=TRUE,boundary_lines=blines,draw_boundary=TRUE,boundary_holes=c(1,3),
+#' 															end_side=3)
+#' 		turtle_right(180)
+#' 		blines <- c(1,2,4)
+#' 	}
+#' })
 #'
 #' @export
 sierpinski_trapezoid_maze <- function(depth,unit_len=4L,clockwise=TRUE,start_from=c('midpoint','corner'),
-																			color1='black',color2='gray40',
+																			color1='black',color2='gray40',flip_color_parts=1,
 																			draw_boundary=FALSE,num_boundary_holes=2,boundary_lines=TRUE,
 																			boundary_holes=NULL,boundary_hole_color=NULL,boundary_hole_locations=NULL,
 																			end_side=1) {
@@ -89,13 +130,8 @@ sierpinski_trapezoid_maze <- function(depth,unit_len=4L,clockwise=TRUE,start_fro
 
 	multiplier <- ifelse(clockwise,1,-1)
 
+# get to midpoint
 	if (start_from=='corner') { turtle_forward(distance=unit_len * num_segs) }
-
-	#cat(str(list(dep=depth,
-							 #num_segs=num_segs,
-							 #holes=boundary_holes,
-							 #holoc=boundary_hole_locations,
-							 #bline=boundary_lines)))
 
 	if (depth > 0) {
 
@@ -112,111 +148,218 @@ sierpinski_trapezoid_maze <- function(depth,unit_len=4L,clockwise=TRUE,start_fro
 			boundary_hole_locations <- sapply(num_segs * c(2,1,1,1),
 																				function(x) { sample.int(x,size=1) })
 		}
-		#cat('picking: ',
-				#str(list(holes=holes,
-								 #holoc=boundary_hole_locations,
-								 #bline=boundary_lines)))
+
+		start_other_side <- (3 %in% flip_color_parts) && !(1 %in% flip_color_parts)
+
+		if (start_other_side) {
+			# this is the more traditional Sierpinski Triangle base.
+			turtle_forward(num_segs * unit_len)
+			.turn_right(multiplier * 120)
+			turtle_forward(num_segs * unit_len)
+			.turn_right(multiplier * 60)
+
+			# now start on number 3 ... 
+			blines <- rep(FALSE,4)
+			blines[1] <- boundary_lines[3]
+			bholes <- rep(0,4)
+			bholes[1] <- holes[3]
+			bhc <- rep('clear',4)
+			bhc[1] <- boundary_hole_color[3]
+			sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='corner',
+																color1=ifelse(3 %in% flip_color_parts,color2,color1),color2=color2,flip_color_parts=flip_color_parts,
+																draw_boundary=TRUE,
+																num_boundary_holes=NULL,
+																boundary_lines=blines,
+																boundary_holes=bholes,
+																boundary_hole_locations=c(boundary_hole_locations[3],0,0,0),
+																boundary_hole_color=bhc,
+																end_side=1)
+
+			turtle_forward(num_segs * unit_len)
+			.turn_right(multiplier * 60)
+
+			# determine inner holes
+			starts <- c(4,1,1,1,2)
+			ends   <- c(3,2,3,4,3)
+			which_holes <- .span_tree(starts,ends)
+
+			inner_lines <- c(FALSE,FALSE,FALSE,TRUE,
+											 FALSE,TRUE,TRUE,TRUE,
+											 FALSE,TRUE,FALSE,FALSE)
+			inner_holes <- rep(FALSE,length(inner_lines))
+			inner_holes[which(inner_lines)[which_holes]] <- TRUE
+
+			blines <- inner_lines[1:4]
+			blines[1] <- boundary_lines[4]
+			blines[2] <- boundary_lines[1]
+			bholes <- inner_holes[1:4]
+			bholes[1] <- holes[4]
+			bholes[2] <- holes[1] & (boundary_hole_locations[1] <= (num_segs/2))
+			bhc <- rep('clear',4)
+			bhc[1] <- boundary_hole_color[4]
+			bhc[2] <- boundary_hole_color[1]
+			sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='corner',
+																color1=ifelse(4 %in% flip_color_parts,color2,color1),color2=color2,flip_color_parts=flip_color_parts,
+																draw_boundary=TRUE,
+																num_boundary_holes=NULL,
+																boundary_lines=blines,
+																boundary_holes=bholes,
+																boundary_hole_locations=c(boundary_hole_locations[4],boundary_hole_locations[1],0,0),
+																boundary_hole_color=bhc,
+																end_side=1)
+
+			turtle_forward(num_segs * unit_len)
+			.turn_right(multiplier * 120)
+			turtle_forward(num_segs * unit_len)
 
 
+			bhole1 <- boundary_hole_locations[1] - (num_segs/2)
 
-		bhc <- rep('clear',4)
-		bhc[1] <- boundary_hole_color[1]
-		
-		# recurse.
-		bhole1 <- boundary_hole_locations[1] - (num_segs/2)
-		# the inner hole is twice color2 btw:
-		# if you want to be weird, sub in color2=color1. does not look as good tho
-		sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='midpoint',
-															color1=color2,color2=color2,
-														 	draw_boundary=TRUE,
-															num_boundary_holes=NULL,
-															boundary_lines=c(boundary_lines[1],rep(FALSE,3)),
-															boundary_holes=c(holes[1] & (bhole1 > 0) & (bhole1 <= num_segs),rep(FALSE,3)),
-															boundary_hole_locations=c(max(0,bhole1),0,0,0),
-															boundary_hole_color=bhc,
-															end_side=1)
+			blines <- inner_lines[5:8]
+			blines[1] <- boundary_lines[1]
+			bholes <- inner_holes[5:8]
+			bholes[1] <- holes[1] & (bhole1 > 0) & (bhole1 <= num_segs)
+			bhc <- rep('clear',4)
+			bhc[1] <- boundary_hole_color[1]
+			
+			# recurse.
+			sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='midpoint',
+																color1=ifelse(1 %in% flip_color_parts,color2,color1),color2=color2,flip_color_parts=flip_color_parts,
+																draw_boundary=TRUE,
+																num_boundary_holes=NULL,
+																boundary_lines=blines,
+																boundary_holes=bholes,
+																boundary_hole_locations=c(max(0,bhole1),0,0,0),
+																boundary_hole_color=bhc,
+																end_side=1)
 
-		# determine inner holes
-		starts <- c(2,2,3,4,4)
-		ends   <- c(3,1,1,1,3)
-		which_holes <- .span_tree(starts,ends)
+			turtle_forward(num_segs * unit_len)
+			.turn_right(multiplier * 120)
 
-		inner_lines <- c(FALSE,TRUE,TRUE,FALSE,
-										 FALSE,FALSE,TRUE,FALSE,
-										 FALSE,FALSE,TRUE,TRUE)
-		inner_holes <- rep(FALSE,length(inner_lines))
-		inner_holes[which(inner_lines)[which_holes]] <- TRUE
+			blines <- inner_lines[9:12]
+			blines[1] <- boundary_lines[2]
+			blines[4] <- boundary_lines[1]
+			bhole2 <- bhole1 - num_segs
+			bholes <- inner_holes[9:12]
+			bholes[1] <- holes[2] 
+			bholes[4] <- holes[1] & (bhole2 > 0)
+			bhc <- rep('clear',4)
+			bhc[1] <- boundary_hole_color[2]
+			bhc[4] <- boundary_hole_color[1]
 
-		# have at it
+			sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='corner',
+																color1=ifelse(2 %in% flip_color_parts,color2,color1),color2=color2,flip_color_parts=flip_color_parts,
+																draw_boundary=TRUE,
+																num_boundary_holes=NULL,
+																boundary_lines=blines,
+																boundary_holes=bholes,
+																boundary_hole_locations=c(boundary_hole_locations[2],0,0,bhole2),
+																boundary_hole_color=bhc,
+																end_side=1)
 
-		turtle_forward(num_segs * unit_len)
-		.turn_right(multiplier * 120)
+			.turn_left(multiplier * 120)
+			turtle_backward(unit_len * num_segs)
+			#turtle_right(180)
+			#turtle_backward(unit_len * num_segs / 2)
+		} else {
+			bhc <- rep('clear',4)
+			bhc[1] <- boundary_hole_color[1]
+			
+			# recurse.
+			bhole1 <- boundary_hole_locations[1] - (num_segs/2)
+			sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='midpoint',
+																color1=ifelse(1 %in% flip_color_parts,color2,color1),color2=color2,flip_color_parts=flip_color_parts,
+																draw_boundary=TRUE,
+																num_boundary_holes=NULL,
+																boundary_lines=c(boundary_lines[1],rep(FALSE,3)),
+																boundary_holes=c(holes[1] & (bhole1 > 0) & (bhole1 <= num_segs),rep(FALSE,3)),
+																boundary_hole_locations=c(max(0,bhole1),0,0,0),
+																boundary_hole_color=bhc,
+																end_side=1)
 
-		blines <- inner_lines[1:4]
-		blines[1] <- boundary_lines[2]
-		blines[4] <- boundary_lines[1]
-		bhole2 <- bhole1 - num_segs
-		bholes <- inner_holes[1:4]
-		bholes[1] <- holes[2] 
-		bholes[4] <- holes[1] & (bhole2 > 0)
-		bhc <- rep('clear',4)
-		bhc[1] <- boundary_hole_color[2]
-		bhc[4] <- boundary_hole_color[1]
+			# determine inner holes
+			starts <- c(2,2,3,4,4)
+			ends   <- c(3,1,1,1,3)
+			which_holes <- .span_tree(starts,ends)
 
-		sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='corner',
-															color1=color1,color2=color2,
-														 	draw_boundary=TRUE,
-															num_boundary_holes=NULL,
-															boundary_lines=blines,
-															boundary_holes=bholes,
-															boundary_hole_locations=c(boundary_hole_locations[2],0,0,bhole2),
-															boundary_hole_color=bhc,
-															end_side=1)
+			inner_lines <- c(FALSE,TRUE,TRUE,FALSE,
+											 FALSE,FALSE,TRUE,FALSE,
+											 FALSE,FALSE,TRUE,TRUE)
+			inner_holes <- rep(FALSE,length(inner_lines))
+			inner_holes[which(inner_lines)[which_holes]] <- TRUE
 
-		turtle_forward(num_segs * unit_len)
-		.turn_right(multiplier * 60)
+			# have at it
 
-		blines <- inner_lines[5:8]
-		blines[1] <- boundary_lines[3]
-		bholes <- inner_holes[5:8]
-		bholes[1] <- holes[3]
-		bhc <- rep('clear',4)
-		bhc[1] <- boundary_hole_color[3]
-		sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='corner',
-															color1=color1,color2=color2,
-														 	draw_boundary=TRUE,
-															num_boundary_holes=NULL,
-															boundary_lines=blines,
-															boundary_holes=bholes,
-															boundary_hole_locations=c(boundary_hole_locations[3],0,0,0),
-															boundary_hole_color=bhc,
-															end_side=1)
+			turtle_forward(num_segs * unit_len)
+			.turn_right(multiplier * 120)
 
-		turtle_forward(num_segs * unit_len)
-		.turn_right(multiplier * 60)
+			blines <- inner_lines[1:4]
+			blines[1] <- boundary_lines[2]
+			blines[4] <- boundary_lines[1]
+			bhole2 <- bhole1 - num_segs
+			bholes <- inner_holes[1:4]
+			bholes[1] <- holes[2] 
+			bholes[4] <- holes[1] & (bhole2 > 0)
+			bhc <- rep('clear',4)
+			bhc[1] <- boundary_hole_color[2]
+			bhc[4] <- boundary_hole_color[1]
 
-		blines <- inner_lines[9:12]
-		blines[1] <- boundary_lines[4]
-		blines[2] <- boundary_lines[1]
-		bholes <- inner_holes[9:12]
-		bholes[1] <- holes[4]
-		bholes[2] <- holes[1] & (boundary_hole_locations[1] <= (num_segs/2))
-		bhc <- rep('clear',4)
-		bhc[1] <- boundary_hole_color[4]
-		bhc[2] <- boundary_hole_color[1]
-		sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='corner',
-															color1=color1,color2=color2,
-														 	draw_boundary=TRUE,
-															num_boundary_holes=NULL,
-															boundary_lines=blines,
-															boundary_holes=bholes,
-															boundary_hole_locations=c(boundary_hole_locations[4],boundary_hole_locations[1],0,0),
-															boundary_hole_color=bhc,
-															end_side=1)
+			sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='corner',
+																color1=ifelse(2 %in% flip_color_parts,color2,color1),color2=color2,flip_color_parts=flip_color_parts,
+																draw_boundary=TRUE,
+																num_boundary_holes=NULL,
+																boundary_lines=blines,
+																boundary_holes=bholes,
+																boundary_hole_locations=c(boundary_hole_locations[2],0,0,bhole2),
+																boundary_hole_color=bhc,
+																end_side=1)
 
-		turtle_forward(num_segs * unit_len)
-		.turn_right(multiplier * 120)
-		turtle_forward(num_segs * unit_len)
+			turtle_forward(num_segs * unit_len)
+			.turn_right(multiplier * 60)
+
+			blines <- inner_lines[5:8]
+			blines[1] <- boundary_lines[3]
+			bholes <- inner_holes[5:8]
+			bholes[1] <- holes[3]
+			bhc <- rep('clear',4)
+			bhc[1] <- boundary_hole_color[3]
+			sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='corner',
+																color1=ifelse(3 %in% flip_color_parts,color2,color1),color2=color2,flip_color_parts=flip_color_parts,
+																draw_boundary=TRUE,
+																num_boundary_holes=NULL,
+																boundary_lines=blines,
+																boundary_holes=bholes,
+																boundary_hole_locations=c(boundary_hole_locations[3],0,0,0),
+																boundary_hole_color=bhc,
+																end_side=1)
+
+			turtle_forward(num_segs * unit_len)
+			.turn_right(multiplier * 60)
+
+			blines <- inner_lines[9:12]
+			blines[1] <- boundary_lines[4]
+			blines[2] <- boundary_lines[1]
+			bholes <- inner_holes[9:12]
+			bholes[1] <- holes[4]
+			bholes[2] <- holes[1] & (boundary_hole_locations[1] <= (num_segs/2))
+			bhc <- rep('clear',4)
+			bhc[1] <- boundary_hole_color[4]
+			bhc[2] <- boundary_hole_color[1]
+			sierpinski_trapezoid_maze(unit_len=unit_len,depth=depth-1,clockwise=clockwise,start_from='corner',
+																color1=ifelse(4 %in% flip_color_parts,color2,color1),color2=color2,flip_color_parts=flip_color_parts,
+																draw_boundary=TRUE,
+																num_boundary_holes=NULL,
+																boundary_lines=blines,
+																boundary_holes=bholes,
+																boundary_hole_locations=c(boundary_hole_locations[4],boundary_hole_locations[1],0,0),
+																boundary_hole_color=bhc,
+																end_side=1)
+
+			turtle_forward(num_segs * unit_len)
+			.turn_right(multiplier * 120)
+			turtle_forward(num_segs * unit_len)
+		}
+
 	} else {
 		if (draw_boundary) {
 			turtle_backward(distance=unit_len * num_segs)
@@ -232,6 +375,7 @@ sierpinski_trapezoid_maze <- function(depth,unit_len=4L,clockwise=TRUE,start_fro
 	}
 
 	# move to ending side
+	# this might still be broken...
 	if ((end_side != 1) && (!is.null(end_side))) {
 		turtle_backward(distance=unit_len * num_segs)
 		molens <-  num_segs * c(2,1,1,1)
@@ -270,6 +414,37 @@ sierpinski_trapezoid_maze <- function(depth,unit_len=4L,clockwise=TRUE,start_fro
 ##dev.copy(png,height=8,width=8,units='in',res=300,'siertrap.png')
 ##dev.off()
 
+
+#require(TurtleGraphics)
+#turtle_init(1700,900,mode='clip')
+#turtle_hide()
+#turtle_up()
+#turtle_do({
+  #turtle_setpos(845,450)
+  #turtle_setangle(0)
+	#blines <- c(1,2,4)
+	#for (dep in seq(from=6,to=0)) {
+		#sierpinski_trapezoid_maze(unit_len=7,depth=dep,color1='black',color2='green',
+															#flip_color_parts=2,
+															#clockwise=FALSE,boundary_lines=blines,draw_boundary=TRUE,boundary_holes=c(1,3),
+															#end_side=3)
+		#turtle_right(180)
+		#blines <- c(1,2,4)
+	#}
+  #turtle_setpos(855,450)
+  #turtle_setangle(0)
+	#blines <- c(1,2,4)
+	#for (dep in seq(from=6,to=0)) {
+		#sierpinski_trapezoid_maze(unit_len=7,depth=dep,color1='black',color2='green',
+															#flip_color_parts=3,
+															#clockwise=TRUE,boundary_lines=blines,draw_boundary=TRUE,boundary_holes=c(1,3),
+															#end_side=3)
+		#turtle_right(180)
+		#blines <- c(1,2,4)
+	#}
+#})
+#dev.copy(png,height=8,width=8,units='in',res=300,'sier_werid.png')
+#dev.off()
 
 #for vim modeline: (do not edit)
 # vim:fdm=marker:fmr=FOLDUP,UNFOLD:cms=#%s:syn=r:ft=r
